da80fabb7e9609924ca34d092d6e4634d8a144a1
adodin <dodin.amro@gmail.com>
2022-04-28 19:19:58 -0700

    Fixed Cl2- files

--- Na2Cl/sub-min.sh
+++ Na2Cl/sub-min.sh
@@ -2,16 +2,16 @@
 #$ -cwd
 #$ -pe mpi 8
 #$ -j y
-#$ -N Na2Cl-min
+#$ -N NaCl-min
 #$ -V
 #$ -q all.q
 
 let "SEED = ${JOB_ID}"
 echo "SEED: $SEED"
-DATADIR=~/DATA/Ion-Pairing/Na2-Cl/replica$1/
+DATADIR=~/DATA/Ion-Pairing/NaCl/replica$1/
 mkdir $DATADIR
 
 mpirun -np 8 lmp -in min.NaCl.lmp -v DATADIR ${DATADIR} -v SEED $SEED \
   -log ${DATADIR}log.NaCl.min
 
-mv ${JOB_NAME}.o${JOB_ID}.${SGE_TASK_ID}
+mv ${JOB_NAME}.o${JOB_ID} ${DATADIR}.
