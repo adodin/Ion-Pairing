3aff47e216e89d815965ef362cca1efb0875c9c2
amr <dodin.amro@gmail.com>
2022-09-06 15:37:57 -0700

    Updated Subs cript requests

--- Universal/sub-run.sh
+++ Universal/sub-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #$ -cwd
-#$ -pe mpi 8
+#$ -pe mpi 24
 #$ -j y
 #$ -N IP-run
 #$ -V
-#$ -q regular
+#$ -q all.q
 #$ -hold_jid IP-min
 
 # REQUIRED ARGS: CATION ANION BC REPLICA_LABEL
@@ -40,7 +40,7 @@ echo "Optional Arguments: $@"
 mkdir $DATADIR
 
 # Run Job
-mpirun -np 8 lmp -in run.lmp -v DATADIR ${DATADIR} -v SEED $SEED \
+mpirun -np 24 lmp -in run.lmp -v DATADIR ${DATADIR} -v SEED $SEED \
   -v cation $cation -v anion $anion -v BC $BC $@
 
 # Move Log File to DATADIR
