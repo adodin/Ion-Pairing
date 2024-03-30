dde8230b61fd5c614e202bfc755df96337404cee
amr <adodin@mit.edu>
2024-03-29 23:43:55 -0700

    Added ECC charge labeling

--- LAMMPS/sub-density.sh
+++ LAMMPS/sub-density.sh
@@ -5,4 +5,4 @@
 #$ -V
 #$ -N rho_water
 
-mpirun -np ${NSLOTS} lmp -in rerun-density.lmp -v datafile ~/DATA/Ion-Pairing/Na.Cl/insulator/replica2/data.insulator.tip4p.Na.Cl.z.a1.0.k.2.5.r.a1.c1.2.5.k.10.0.eq  -v trajfile ~/DATA/Ion-Pairing/Na.Cl/insulator/replica2/prod.insulator.tip4p.Na.Cl.z.a1.0.k.2.5.r.a1.c1.2.5.k.10.0.lammpstrj  -v trajEvery 1000 -v outFreq 1000000 -v last 1000000 -v DATADIR ~/DATA/Ion-Pairing/Na.Cl/insulator/replica2/ -v label water.Drude.insulator.swm4ndp.Na.Cl
+mpirun -np ${NSLOTS} lmp -in rerun-density.lmp -v datafile $1  -v trajfile $2  -v trajEvery 1000 -v outFreq $3 -v last $3 -v DATADIR $4 -v label $5 -v dz 2
--- LAMMPS/sub-run.sh
+++ LAMMPS/sub-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -pe mpi 8
+#$ -pe smp 24
 #$ -j y
 #$ -N IP-run
 #$ -V
