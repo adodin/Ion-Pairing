602e227fbd54c33eb5a254829a9d4565cf71d9ff
adodin <dodin.amro@gmail.com>
2024-05-23 16:21:06 -0700

    density now requires types

--- LAMMPS/sub-density.sh
+++ LAMMPS/sub-density.sh
@@ -5,4 +5,4 @@
 #$ -V
 #$ -N rho_water
 
-mpirun -np ${NSLOTS} lmp -in rerun-density.lmp -v datafile $1  -v trajfile $2  -v trajEvery 1000 -v outFreq $3 -v last $3 -v DATADIR $4 -v label $5 -v dz 2
+mpirun -np ${NSLOTS} lmp -in rerun-density.lmp -v datafile $1  -v trajfile $2  -v trajEvery 1000 -v outFreq $3 -v last $3 -v DATADIR $4 -v label $5 -v dz 2 -v types ${@:6}
